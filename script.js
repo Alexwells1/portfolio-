98575a2285393c031800ff68097e591caec4d094
Alexwells1 <122620423+Alexwells1@users.noreply.github.com>
2025-02-16 21:37:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-// script.js
-document.addEventListener("DOMContentLoaded", function() {
-    const header = document.querySelector(".sticky-header");
-    const heroSection = document.querySelector(".hero");
-
-    function updateHeader() {
-        const heroBottom = heroSection.getBoundingClientRect().bottom;
-        
-        if (heroBottom < window.innerHeight / 2) {
-            header.style.bottom = "0";
-        } else {
-            header.style.bottom = "-100px";
-        }
-    }
-
-    window.addEventListener("scroll", updateHeader);
+document.getElementById('dark-mode-toggle').addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
 });
+
